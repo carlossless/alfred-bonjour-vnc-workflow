cf1444af78023dfc86e75352f8dc4d07e310ebba
Karolis Stasaitis <contact@carlossless.io>
2020-10-19 10:09:51 +0200

    trying fuzzy-match

--- src/main.rb
+++ src/main.rb
@@ -5,8 +5,10 @@ require './bundle/bundler/setup'
 require 'dnssd'
 require 'timeout'
 require 'json'
+require 'fuzzy_match'
 
 replies = []
+query = ARGV.first && ARGV.first.strip
 
 begin
   Timeout.timeout 0.01 do
@@ -21,12 +23,23 @@ hostnames = replies.map(&:fullname).uniq.map do |service_name|
   service_name[0..-1]
 end
 
+hostnames = FuzzyMatch.new(hostnames).find_all(query) unless query.nil? || query.empty?
+
 items = hostnames.map do |host|
   {
     title: host,
     subtitle: "vnc://#{host}",
     arg: "vnc://#{host}"
   }
+end
+
+query = ARGV.first.strip
+if !query.nil? && !query.empty?
+  items.append({
+    title: query,
+    subtitle: "vnc://#{query}",
+    arg: "vnc://#{query}"
+  })
 end
 
 result = { items: items }.to_json
